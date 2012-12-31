47e72f2f1fe4d41309d5bc9b4efa6fe3200e188e
Lalit Maganti <lalitmaganti@gmail.com>
2012-12-31 19:12:46 +0000

    Small fixes
    Change-Id: I852f1fe366277d5b4a392bfa0fdf76b2ae42e89f

--- mm-video/vidc/vdec/Android.mk
+++ mm-video/vidc/vdec/Android.mk
@@ -27,6 +27,7 @@ libOmxVdec-def += -DPROCESS_EXTRADATA_IN_OUTPUT_PORT
 libOmxVdec-def += -DTEST_TS_FROM_SEI
 endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+libOmxVdec-def += -DUSE_ION
 libOmxVdec-def += -DMAX_RES_1080P
 libOmxVdec-def += -DMAX_RES_1080P_EBI
 libOmxVdec-def += -DPROCESS_EXTRADATA_IN_OUTPUT_PORT
@@ -42,10 +43,6 @@ libOmxVdec-def += -DMAX_RES_720P
 endif
 
 libOmxVdec-def += -D_ANDROID_ICS_
-
-#ifeq ($(TARGET_USES_ION),true)
-#libOmxVdec-def += -DUSE_ION
-#endif
 
 # ---------------------------------------------------------------------------------
 # 			Make the Shared library (libOmxVdec)
