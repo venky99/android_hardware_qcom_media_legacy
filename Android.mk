94b50b52b594fb0d874ccbdfa3bc4842db3551f9
Adam Farden <adam@farden.cz>
2013-01-08 15:47:15 +0100

    Allow building with msm7x27a
    Change-Id: Idad4310d91cb47eb4b4f6fe4839561fea93a6cf4

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(call all-subdir-makefiles)
 endif
 
 QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm7x27,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm7x27 msm7x27a,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
--- mm-video/vidc/vdec/Android.mk
+++ mm-video/vidc/vdec/Android.mk
@@ -38,6 +38,9 @@ libOmxVdec-def += -DMAX_RES_1080P_EBI
 libOmxVdec-def += -DPROCESS_EXTRADATA_IN_OUTPUT_PORT
 libOmxVdec-def += -D_COPPER_
 endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
+libOmxVdec-def += -DMAX_RES_720P
+endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x27)
 libOmxVdec-def += -DMAX_RES_720P
 endif
